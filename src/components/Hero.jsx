96e9d7bb60e645b3ff31087ec902184e20032006
ahmedhany <elgenius10000@gmail.com>
2023-04-01 17:18:03 +0200

    add about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,17 +30,34 @@ const Hero = () => {
       <ComputersCanvas />
       {/* </div> */}
 
-      <div className="absolute sm:top-0 sm:right-0  lg:top-2 lg:right-4">
+      <div className="absolute  xs:top-[1050px] lg:block  lg:top-2 lg:right-4">
         <img
           src={myImg}
           alt="my photo"
-          className="object-contain w-[460px] relative"
+          className="object-contain   w-[460px] relative"
         />
         <img
           src={fairGif}
           alt="right hand "
-          className="absolute object-contain w-92   top-[155px] right-[-91px]"
+          className="absolute object-contain w-92 sm:hidden   top-[155px] right-[-91px]"
         />
+      </div>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   );
